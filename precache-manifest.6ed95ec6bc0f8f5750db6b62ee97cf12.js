c187b1afff2d3918d33302d12a76324f17853865
theomnipotentbeing <72243555+theomnipotentbeing@users.noreply.github.com>
2020-10-16 20:58:12 +0700

    Initial Commit

---
+++ precache-manifest.6ed95ec6bc0f8f5750db6b62ee97cf12.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45aa64e793b6af4caf66b80f0e4a4b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24b2d3d7db76f31a097",
+    "url": "/static/js/2.c2801fa7.chunk.js"
+  },
+  {
+    "revision": "8b23cc64f7a6e5ec6ca5b5224be34b56",
+    "url": "/static/js/2.c2801fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8908c586c2bb547cc753",
+    "url": "/static/js/main.21f79e6a.chunk.js"
+  },
+  {
+    "revision": "492123e915d597e7b084",
+    "url": "/static/js/runtime-main.4e252cfa.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "a10d73333f2e7e1ced177116aa57f2a8",
+    "url": "/static/media/logo.a10d7333.svg"
+  },
+  {
+    "revision": "a10d73333f2e7e1ced177116aa57f2a8",
+    "url": "/static/media/logo_white.a10d7333.svg"
+  }
+]);
